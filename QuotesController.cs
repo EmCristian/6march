81f275e350c18d6910e234f332994634b5da4e42
EmCristian <100667674+EmCristian@users.noreply.github.com>
2022-03-06 13:15:56 -0800

    Add files via upload

---
+++ QuotesController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi3.Data;
+using QuotesApi3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesApi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+        public QuotesController(QuotesDbContext quotesDbContext)
+        {
+            _quotesDbContext = quotesDbContext;
+        }
+        // GET: api/<QuotesController>
+        [HttpGet]
+        public IActionResult Get(string sort)
+        {
+            IQueryable<Quote> quotes;
+            switch(sort)
+            {
+                case "desc":
+                    quotes = _quotesDbContext.Quotes.OrderByDescending(quote => quote.CreatedAt);
+                    break;
+                case "asc":
+                    quotes = _quotesDbContext.Quotes.OrderBy(quote => quote.CreatedAt);
+                    break;
+                default:
+                    quotes = _quotesDbContext.Quotes;
+                    break;
+            }
+            return Ok(quotes);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult PagingQuote(int? pageNumber, int? pageSize)
+        {
+            var quotes = _quotesDbContext.Quotes;
+            var currentPageNumber = pageNumber ?? 1;
+            var currentPageSize = pageSize ?? 5;
+
+            return Ok(quotes.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize));
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult SearchQuote(string type)
+        {
+            var quotes = _quotesDbContext.Quotes.Where(quote => quote.Type.StartsWith(type));
+            return Ok(quotes);
+        }
+
+        // GET api/<QuotesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if (quote == null)
+            {
+                return NotFound("No quote found with this id");
+            }
+            else
+            {
+                return Ok(quote);
+            }
+        }
+
+        // api/Quotes/Test/1
+        [HttpGet("[action]/{id}")]
+        public int Test(int id)
+        {
+            return id;
+        }
+
+        // POST api/<QuotesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Quote quote)
+        {
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<QuotesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Quote quote)
+        {
+            var entity = _quotesDbContext.Quotes.Find(id);
+            if (entity == null)
+            {
+                return NotFound("No record found with this id");
+            }
+            else
+            {
+                entity.Title = quote.Title;
+                entity.Author = quote.Author;
+                entity.Description = quote.Description;
+                entity.Type = quote.Type;
+                entity.CreatedAt = quote.CreatedAt;
+                _quotesDbContext.SaveChanges();
+
+                return Ok("Record updated successfully!");
+            }
+        }
+
+        // DELETE api/<QuotesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if (quote == null)
+            {
+                return NotFound("Quote not found with this id");
+            }
+            else
+            {
+                _quotesDbContext.Remove(quote);
+                _quotesDbContext.SaveChanges();
+                return Ok("Quote has been deleted!");
+            }
+        }
+    }
+}
